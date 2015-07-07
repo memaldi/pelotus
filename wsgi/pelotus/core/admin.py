7ae8c50ae2595914a8f7dc9b55a121d46b3bd70f
Mikel Emaldi <m.emaldi@gmail.com>
2015-07-07 22:16:16 +0200

    Working on admin panel

--- wsgi/pelotus/core/admin.py
+++ wsgi/pelotus/core/admin.py
@@ -2,20 +2,38 @@ from django.contrib import admin
 from core.models import *
 
 # Register your models here.
-admin.site.register(Season)
-admin.site.register(Community)
-admin.site.register(Competition)
-#admin.site.register(Team)
-admin.site.register(MatchDay)
-admin.site.register(Match)
-admin.site.register(Player)
-admin.site.register(PlayerBelongsToTeam)
-admin.site.register(UserProfile)
-admin.site.register(UserAdministration)
-admin.site.register(Bet)
-admin.site.register(GoalsBet)
+#admin.site.register(Season)
+# admin.site.register(Community)
+# admin.site.register(Competition)
+# admin.site.register(MatchDay)
+# admin.site.register(Match)
+# #admin.site.register(PlayerBelongsToTeam)
+# admin.site.register(UserProfile)
+# admin.site.register(UserAdministration)
+# admin.site.register(Bet)
+# admin.site.register(GoalsBet)
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description', 'start_date', 'end_date')
+
+class PlayerBelongsToTeamInLine(admin.TabularInline):
+	model = PlayerBelongsToTeam
+	extra = 1
+
+class TeamInSeasonAdmin(admin.TabularInline):
+	model = TeamInSeason
+	extra = 1
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-	list_display = ('name','spanish_league', 'champions_league', 'uefa_league', 'kings_cup',)
+	list_display = ('name', 'current_season',)
 	search_fields = ['name']
+	inlines = (TeamInSeasonAdmin,)
+	# inlines = (PlayerBelongsToTeamInLine,)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+	search_fields = ['name']
+	inlines = (PlayerBelongsToTeamInLine,)
--- wsgi/pelotus/core/models.py
+++ wsgi/pelotus/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 
@@ -7,7 +8,12 @@ from django.contrib.auth.models import User
 class Season(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
-    description = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+    teams = models.ManyToManyField('Team', through='TeamInSeason')
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
 
 # A group of users playing to Pelotus
 class Community(models.Model):
@@ -18,16 +24,6 @@ class Community(models.Model):
 class Competition(models.Model):
     season = models.ForeignKey('Season')
     community = models.ForeignKey('Community')
-
-class Team(models.Model):
-    name = models.CharField(max_length=20)
-    spanish_league = models.BooleanField(default=True)
-    uefa_league = models.BooleanField(default=False)
-    champions_league = models.BooleanField(default=False)
-    kings_cup = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['name']
 
 class MatchDay(models.Model):
     number = models.IntegerField()
@@ -44,8 +40,38 @@ class Match(models.Model):
 class Player(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Team(models.Model):
+    name = models.CharField(max_length=20)
+
+    def current_season(self):
+        return self.teaminseason_set.filter(season__start_date__lte=datetime.date.today(), season__end_date__gte=datetime.date.today()).first().season.name
+
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class TeamInSeason(models.Model):
+    team = models.ForeignKey('Team')
+    season = models.ForeignKey('Season')
+    spanish_league = models.BooleanField(default=True)
+    uefa_league = models.BooleanField(default=False)
+    champions_league = models.BooleanField(default=False)
+    kings_cup = models.BooleanField(default=False)
+    players = models.ManyToManyField(Player, through='PlayerBelongsToTeam')
+
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.team.name
+
 class PlayerBelongsToTeam(models.Model):
     player = models.ForeignKey('Player')
+    team_in_season = models.ForeignKey('TeamInSeason')
     season = models.ForeignKey('Season')
     POSITION_CHOICES = (
         ('GK', 'GoalKeeper'),
@@ -76,4 +102,3 @@ class GoalsBet(models.Model):
     forward = models.ForeignKey('Player', related_name='forward')
     midfield = models.ForeignKey('Player', related_name='midfield')
     defense = models.ForeignKey('Player', related_name='defense')
-
