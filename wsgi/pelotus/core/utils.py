50f3b70c10433da8f63ce0d4ed624a4aec69c774
Mikel Emaldi <m.emaldi@gmail.com>
2015-08-04 12:06:17 +0200

    Calculating scorers points

--- wsgi/pelotus/core/utils.py
+++ wsgi/pelotus/core/utils.py
@@ -1,5 +1,5 @@
 # Util classes
-from core.models import MatchDay, Competition, Bet
+from core.models import MatchDay, Competition, Bet, GoalsBet, PlayerGoal
 
 
 def match_day_ranking(match_day):
@@ -20,5 +20,19 @@ def get_user_match_day_points(user, match_day, competition):
 
         if (bet.home_goals == bet.match.home_goals and bet.foreign_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
             user_points += 3
+
+    goals_bet = GoalsBet.objects.filter(user=user, match_day=match_day).first()
+    if goals_bet != None:
+        player_goal = PlayerGoal.objects.filter(match_day=match_day, player=goals_bet.forward).first()
+        if player_goal != None:
+            user_points += player_goal.goals
+
+        player_goal = PlayerGoal.objects.filter(match_day=match_day, player=goals_bet.midfield).first()
+        if player_goal != None:
+            user_points += player_goal.goals * 3
+
+        player_goal = PlayerGoal.objects.filter(match_day=match_day, player=goals_bet.defense).first()
+        if player_goal != None:
+            user_points += player_goal.goals * 5
 
     return user_points
