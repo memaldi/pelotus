25447aa7803117a3a010545df14635146b422bff
Mikel Emaldi <m.emaldi@gmail.com>
2015-11-29 20:20:45 +0100

    Fixing bugs

--- wsgi/pelotus/core/utils.py
+++ wsgi/pelotus/core/utils.py
@@ -15,7 +15,7 @@ def match_day_ranking(match_day):
 def get_user_match_day_points(user, match_day, competition):
     user_points = 0
     total_bet = 0
-    mached_bets = 0
+    matched_bets = 0
     for bet in Bet.objects.filter(user=user, match_day=match_day, competition=competition):
         if (bet.home_goals > bet.foreign_goals and bet.match.home_goals > bet.match.foreign_goals) or (bet.home_goals < bet.foreign_goals and bet.match.home_goals < bet.match.foreign_goals) or (bet.home_goals == bet.foreign_goals and bet.match.home_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
            user_points += 5
