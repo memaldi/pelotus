4bc517824f16cc2e5671cbe325e0dcf890c20953
Mikel Emaldi <m.emaldi@gmail.com>
2016-03-26 19:39:28 +0100

    Calculated all global_bets

--- wsgi/pelotus/core/tasks.py
+++ wsgi/pelotus/core/tasks.py
@@ -1,9 +1,10 @@
-from core.models import MatchDay, Competition, Bet, GlobalBet, GlobalResults
+from core.models import MatchDay, Competition, GlobalBet, GlobalResults
 from celeryapp import app
 from django.core.cache import cache
 import utils
 
 TIMEOUT = 259200
+
 
 @app.task
 def match_day_ranking(match_day_id):
@@ -12,17 +13,23 @@ def match_day_ranking(match_day_id):
 
     for competition in competition_points:
         for user in competition_points[competition]:
-            cache.set('competition:{}:match_day:{}:user:{}:points'.format(competition, match_day.id, user), competition_points[competition][user], TIMEOUT)
+            cache.set('competition:{}:match_day:{}:user:{}:points'.format(
+                competition, match_day.id, user),
+                competition_points[competition][user], TIMEOUT)
+
 
 @app.task
 def global_ranking(global_results_id):
     global_results = GlobalResults.objects.get(id=global_results_id)
-    competition_list = Competition.objects.filter(season=global_results.season).all()
+    competition_list = Competition.objects.filter(
+        season=global_results.season).all()
     for competition in competition_list:
-        global_bet_list = GlobalBet.objects.filter(competition=competition).all()
+        global_bet_list = GlobalBet.objects.filter(
+            competition=competition).all()
         for global_bet in global_bet_list:
-            global_points = 0
-            if global_bet.winter_champion is not None:
-                if global_bet.winter_champion == global_results.winter_champion:
-                    global_points += 10
-            cache.set('competiton:{}:global:user:{}:points'.format(competition.id, global_bet.user.id), global_points, TIMEOUT)
+            global_points = utils.get_user_global_points(global_bet.user,
+                                                         competition,
+                                                         global_results)
+
+            cache.set('competiton:{}:global:user:{}:points'.format(
+                competition.id, global_bet.user.id), global_points, TIMEOUT)
--- wsgi/pelotus/core/utils.py
+++ wsgi/pelotus/core/utils.py
@@ -1,5 +1,5 @@
 # Util classes
-from core.models import MatchDay, Competition, Bet, GoalsBet, PlayerGoal, GlobalBet
+from core.models import Competition, Bet, GoalsBet, PlayerGoal, GlobalBet
 
 
 def match_day_ranking(match_day):
@@ -7,19 +7,22 @@ def match_day_ranking(match_day):
     for competition in Competition.objects.filter(season=match_day.season):
         competition_points[competition.id] = {}
         for ua in competition.useradministration_set.all():
-            user_points = get_user_match_day_points(ua.user, match_day, competition)
+            user_points = get_user_match_day_points(ua.user, match_day,
+                                                    competition)
             competition_points[competition.id][ua.user.id] = user_points
 
     return competition_points
+
 
 def get_user_match_day_points(user, match_day, competition):
     user_points = 0
     total_bet = len(match_day.match_set.all())
     matched_bets = 0
-    for bet in Bet.objects.filter(user=user, match_day=match_day, competition=competition):
+    for bet in Bet.objects.filter(user=user, match_day=match_day,
+                                  competition=competition):
         if (bet.home_goals > bet.foreign_goals and bet.match.home_goals > bet.match.foreign_goals) or (bet.home_goals < bet.foreign_goals and bet.match.home_goals < bet.match.foreign_goals) or (bet.home_goals == bet.foreign_goals and bet.match.home_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
-           user_points += 5
-           matched_bets += 1
+            user_points += 5
+            matched_bets += 1
 
         if (bet.home_goals == bet.match.home_goals and bet.foreign_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
             user_points += 3
@@ -28,25 +31,49 @@ def get_user_match_day_points(user, match_day, competition):
         user_points += 10
 
     goals_bet = GoalsBet.objects.filter(user=user, match_day=match_day).first()
-    if goals_bet != None:
-        player_goal = PlayerGoal.objects.filter(match_day=match_day, player=goals_bet.forward).first()
-        if player_goal != None:
+    if goals_bet is not None:
+        player_goal = PlayerGoal.objects.filter(
+            match_day=match_day, player=goals_bet.forward).first()
+        if player_goal is not None:
             user_points += player_goal.goals
 
-        player_goal = PlayerGoal.objects.filter(match_day=match_day, player=goals_bet.midfield).first()
-        if player_goal != None:
+        player_goal = PlayerGoal.objects.filter(
+            match_day=match_day, player=goals_bet.midfield).first()
+        if player_goal is not None:
             user_points += player_goal.goals * 3
 
-        player_goal = PlayerGoal.objects.filter(match_day=match_day, player=goals_bet.defense).first()
-        if player_goal != None:
+        player_goal = PlayerGoal.objects.filter(
+            match_day=match_day, player=goals_bet.defense).first()
+        if player_goal is not None:
             user_points += player_goal.goals * 5
 
     return user_points
 
+
 def get_user_global_points(user, competition, global_results):
-    global_bet = GlobalBet.objects.filter(competition=competition, user=user).first()
+    global_bet = GlobalBet.objects.filter(competition=competition,
+                                          user=user).first()
     global_points = 0
     if global_bet.winter_champion is not None:
         if global_bet.winter_champion == global_results.winter_champion:
+            global_points += 10
+        if global_bet.kings_cup_champion == global_results.kings_cup_champion:
+            global_points += 10
+        if global_bet.league_champion == global_results.league_champion:
+            global_points += 10
+        if global_bet.uefa_champion == global_results.uefa_champion:
+            global_points += 10
+        if global_bet.champions_league_champion == global_results.champions_league_champion:
+            global_points += 10
+        for team in global_bet.champions_positions.all():
+            if team in global_results.champions_positions.all():
+                global_points += 10
+        for team in global_bet.uefa_positions.all():
+            if team in global_results.uefa_positions.all():
+                global_points += 10
+        for team in global_bet.demotion_positions.all():
+            if team in global_results.demotion_positions.all():
+                global_points += 10
+        if global_bet.best_goalkeeper == global_results.best_goalkeeper:
             global_points += 10
     return global_points
