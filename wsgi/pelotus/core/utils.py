6bd5aefa816688f7545a88312ec511dc82cecc98
Mikel Emaldi <m.emaldi@gmail.com>
2018-11-03 17:42:29 +0100

    Fixing None result bug

--- wsgi/pelotus/core/utils.py
+++ wsgi/pelotus/core/utils.py
@@ -20,12 +20,13 @@ def get_user_match_day_points(user, match_day, competition):
     matched_bets = 0
     for bet in Bet.objects.filter(user=user, match_day=match_day,
                                   competition=competition):
-        if (bet.home_goals > bet.foreign_goals and bet.match.home_goals > bet.match.foreign_goals) or (bet.home_goals < bet.foreign_goals and bet.match.home_goals < bet.match.foreign_goals) or (bet.home_goals == bet.foreign_goals and bet.match.home_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
-            user_points += 5
-            matched_bets += 1
+        if None not in [bet.home_goals, bet.foreign_goals]:
+            if (bet.home_goals > bet.foreign_goals and bet.match.home_goals > bet.match.foreign_goals) or (bet.home_goals < bet.foreign_goals and bet.match.home_goals < bet.match.foreign_goals) or (bet.home_goals == bet.foreign_goals and bet.match.home_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
+                user_points += 5
+                matched_bets += 1
 
-        if (bet.home_goals == bet.match.home_goals and bet.foreign_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
-            user_points += 3
+            if (bet.home_goals == bet.match.home_goals and bet.foreign_goals == bet.match.foreign_goals) and (None not in [bet.home_goals, bet.foreign_goals, bet.match.home_goals, bet.match.foreign_goals]):
+                user_points += 3
 
     if matched_bets >= total_bet:
         user_points += 10
